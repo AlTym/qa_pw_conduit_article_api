73e22e79c632e061e6960edb0fcd63df737f39bd
Alyona <killer2.0.1al@gmail.com>
2026-06-12 01:05:08 +0300

    fixed2

--- tests/articles/create/createArticlesByUnautorizedUser.spec.js
+++ tests/articles/create/createArticlesByUnautorizedUser.spec.js
@@ -3,11 +3,12 @@ import { test } from '../../_fixtures/fixtures';
 test(`Create artile by unautorized user`, async ({
   articlesApi,
   newArticleDataWithoutTags,
+  newUserData
 }) => {
   
   const response = await articlesApi.createArticle(
     newArticleDataWithoutTags,
-    ''
+    newUserData
   );
 
   await articlesApi.assertUnauthorizedResponseCode(response);
